f062234155e48827559d627d81e424ee83ea7f80
Bjoshua25 <bolajijoshua35@gmail.com>
2025-12-03 14:33:50 +0100

    began working on endpoints

--- src/main.py
+++ src/main.py
@@ -1,17 +1,80 @@
 # ------------------ IMPORT LIBRARIES --------------------------
-from fastapi import FastAPI
+import sys
+from pathlib import Path
+from fastapi import FastAPI, Header, HTTPException, status, Depends
+from typing import Annotated
+
+# Handle directory
+parent_dir = Path(__file__).resolve().parent.parent
+if str(parent_dir) not in sys.path:
+    sys.path.insert(0, str(parent_dir))
+
+# import custom modules
+from src.schemas import LoanPredictionInput, LoanPredictionOutput
+from src.prediction import load_pipeline, predict_default, MODEL_VERSION, pipeline
 
 
 
+
+# ------------ Configuration Pin -------------------------------
+SECRET_API_KEY = "SUPER_SECRET_KEY_12345"
 
 
 # ------------------- FastAPI App ------------------------------
-app = FastAPI()
+app = FastAPI(
+    title = "Loan Default Prediction API",
+    version= MODEL_VERSION,
+    description= "A secure service for predicting loan default risk using a Random Forest Pipeline."
+)
+
+
+
+# ---------- DEPENDENCY INJECTION ------------------------------
+def verify_api_key(x_api_key: str = Header(None)):
+    """
+    Checks for a valid API key in the X-API-key header.
+    """
+
+    if x_api_key is None or x_api_key != SECRET_API_KEY:
+        raise HTTPException (
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail= "Invalid or missing API key in X_API_key header"
+        )
+    return True
+
+AuthDep = Annotated[bool, Depends(verify_api_key)]
 
 
 
 
-# ------------------ Simple Endpoint --------------------------
+# ------------ APPLICATION LIFECYCLE EVENTS -------------------
+@app.on_event("startup")
+async def startup_event():
+    """Run model loading when the application starts."""
+    print("Running application startup routines...")
+    load_pipeline
+    print("Startup complete. API is ready to serve traffic.")
+
+
+
+# -------------- ENDPOINTS -----------------------------------
+
+# health check route
+@app.get("/", tags= ["Health Check"])
+def health_check():
+    status_detail = "Ready" if pipeline is not None else "Model Not Loaded"
+
+    return {
+        "status": status_detail,
+        "api_version": MODEL_VERSION,
+        "model_loaded": pipeline is not None
+    }
+
+
+
+
+
+# ------------------ Simple Endpoint ---------------------------
 @app.get("/")
 def home_endpoint():
 
---
+++ src/prediction.py
@@ -0,0 +1,89 @@
+# ----------------- IMPORT LIBRARIES ---------------------------
+import os
+import sys
+import joblib
+import pandas as pd
+from pathlib import Path
+from schemas import LoanPredictionInput, LoanPredictionOutput
+
+
+# ------------- DIRECTORY HANDLING ----------------------------
+# parent root folder
+parent_dir = Path(__file__).resolve().parent.parent
+
+# save path to system
+if str(parent_dir) not in sys.path:
+    sys.path.insert(0, str(parent_dir))
+
+
+
+
+# ----------- GLOBAL MODEL VARIABLES ---------------------------
+MODEL_PATH = parent_dir / "models" / "best_rf_pipeline.pkl"
+MODEL_VERSION = "1.0.0"
+
+# loaded model (Global)
+pipeline = None
+
+
+
+
+# -------------- LOAD SAVED MODEL -----------------------------
+def load_pipeline():
+    """
+    About: 
+        loads the full scikit-learn peipeline(preprocessor + model) from disk.
+    """
+
+    global pipeline
+    try:
+        pipeline = joblib.load(MODEL_PATH)
+        print(f"Pipeline (Version {MODEL_VERSION}) loaded successfully from {MODEL_PATH}")
+    except FileNotFoundError:
+        print(f"ERROR: Model file not found at {MODEL_PATH}. Check file Path.")
+        pipeline = None
+    except Exception as e:
+        print(f"ERROR loading model: {e}")
+        pipeline = None
+
+
+
+
+
+# ----------- PREDICTION FUNCTION -----------------------------
+def predict_default(input_data: LoanPredictionInput) -> LoanPredictionOutput:
+    """
+    About:
+        Performs the prediction using the loaded model pipeline.
+    Input:
+        input_data (LoanPredictionInput) - The pydantic model containing the raw features.
+    output:
+        LoanPredictionOutput: The structured prediction result.
+    """
+
+    if pipeline is None:
+        raise RuntimeError ("Model Pipeline is Not loaded. Cannot Perform Prediction.")
+    
+    # Convert Pydantic object to DataFrame
+    input_dict = input_data.model_dump()
+    input_df = pd.DataFrame([input_dict])
+
+    # Make probability prediction
+    probability = pipeline.predict_proba(input_df)[:, 1][0]
+
+    # Get Binary Prediction
+    prediction = pipeline.predict(input_df[0])
+
+    # Obtin  human-readable result
+    is_default = int(prediction)
+    recommendation = (
+        "High Risk: Loan Default Predicted" if is_default == 1
+        else "Low Risk: Loan Repaymnet Predicted"
+    )
+
+    return LoanPredictionOutput(
+        is_default = is_default,
+        default_probability = float(probability),
+        recommendation = recommendation,
+        model_version = MODEL_VERSION
+    )
